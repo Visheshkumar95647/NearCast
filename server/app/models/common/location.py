011352215834301a85561cd5e6539ffd8b3c9bb5
Visheshkumar <visheshkumar95647@gmail.com>
2026-09-10 00:43:19 +0530

    Complete Phase 2 database schema

--- server/app/models/common/location.py
+++ server/app/models/common/location.py
@@ -1,13 +1,16 @@
 from geoalchemy2 import Geography
+
 from sqlalchemy.orm import Mapped, mapped_column
 
 
 class LocationMixin:
+
     location: Mapped[object] = mapped_column(
         Geography(
             geometry_type="POINT",
             srid=4326,
-            spatial_index=True,
+            spatial_index=False,
+            # we create spatial index manually in the migration script to avoid issues with Alembic autogenerate
         ),
         nullable=True,
     )
